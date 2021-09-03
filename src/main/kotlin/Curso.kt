3bd74d585f8aeec93bf13fee24214d8f95e392a5
andfrza <andfrza@gmail.com>
2021-09-03 13:23:05 -0300

    Parte E finalizada

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -1,5 +1,6 @@
-class Curso(var nomeCurso:String,var codigoCurso:Int) {
+class Curso(var nomeCurso:String,var codigoCurso:Int, var professorTitular:ProfessorTitular,var professorAdjunto:ProfessorAdjunto, var quantidadeMaximaDeAlunos:Int,var listaDeAlunosMatriculados:MutableList<Aluno>) {
     override fun equals(other: Any?): Boolean {
         return (other is Curso&&this.codigoCurso==other.codigoCurso)
     }
 }
+//Parte E: adcionando os demais atributos de classe.
