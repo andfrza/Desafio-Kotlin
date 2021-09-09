f60a37c75e08ef6f794cdcddde7b9612f531d79d
andfrza <andfrza@gmail.com>
2021-09-09 10:17:49 -0300

    Correção da parte F

--- src/main/kotlin/Matricula.kt
+++ src/main/kotlin/Matricula.kt
@@ -1,12 +1,11 @@
 import java.util.Date
-class Matricula(var aluno: Aluno,var curso: Curso,var dataDeMatricula:Date) {
-    var matricula:MutableList<Matricula> = mutableListOf()
 
-    constructor(aluno:Aluno,curso:Curso,dataDeMatricula: Date,efetivacaoMatricula: Matricula):this(aluno,curso,dataDeMatricula){
-        this.dataDeMatricula=Date()
-        efetivacaoMatricula.matricula.add(this)
+class Matricula {
+    init {
+        var aluno: Aluno
+        var curso: Curso
+        var dataDeMatricula: Date = Date()
     }
+
+    constructor(aluno: Aluno, curso: Curso, dataDeMatricula: Matricula)
 }
-
-//O construtor secundário precisa receber um parâmetro extra, senão dará "referência circular".
-
