5e3cc6f30bd6b3e7978998e8f25770942764913d
andfrza <andfrza@gmail.com>
2021-09-09 10:42:25 -0300

    Parte H finalizada

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -1,6 +1,20 @@
-class Curso(var nomeCurso:String,var codigoCurso:Int, var professorTitular:ProfessorTitular,var professorAdjunto:ProfessorAdjunto, var quantidadeMaximaDeAlunos:Int,var listaDeAlunosMatriculados:MutableList<Aluno>) {
+class Curso(
+    var nomeCurso: String, var codigoCurso: Int, var professorTitular: ProfessorTitular,
+    var professorAdjunto: ProfessorAdjunto, var quantidadeMaximaDeAlunos: Int,
+    var listaDeAlunosMatriculados: MutableList<Aluno>
+) {
     override fun equals(other: Any?): Boolean {
-        return (other is Curso&&this.codigoCurso==other.codigoCurso)
+        return (other is Curso && this.codigoCurso == other.codigoCurso)
+    }
+
+    fun adicionarAluno(aluno: Aluno):Boolean{
+        return listaDeAlunosMatriculados.size<=quantidadeMaximaDeAlunos
+    }
+    fun excluirAluno(aluno: Aluno){
+        listaDeAlunosMatriculados.remove(aluno)
     }
 }
+
+
 //Parte E: adcionando os demais atributos de classe.
+//Parte H: adicionando novos métodos à classe.
